2eba6ea1242654800a2c147712621dcb5a9dc57f
harry.yao <harry.yao@banclogix.com>
2019-04-29 16:27:58 +0800

    State update

--- DesignPatternTest/DesignPatternTest/Behavioral/StateTest.cs
+++ DesignPatternTest/DesignPatternTest/Behavioral/StateTest.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Threading;
-using NUnit.Framework;
 
 namespace DesignPatternTest
 {
@@ -11,16 +11,30 @@ namespace DesignPatternTest
     public class StateTest
     {
         [Test]
-        public void shoppercar_80off()
+        public void Traffic_light()
         {
             var trafficLight = new TrafficLight();
             //trafficLight.Light = new RED();
             //while (true)
             //{
-                trafficLight.Change();
-                trafficLight.Change();
-                trafficLight.Change();
+            trafficLight.Change();
+            trafficLight.Change();
+            trafficLight.Change();
             //}
+        }
+
+        [Test]
+        public void Game_Level_titile()
+        {
+            Player user = new Player();
+            user.level = 1; // 玩家等級
+            user.stateWork(); // 玩家狀態處理
+            user.level = 20;
+            user.stateWork();
+            user.level = 62;
+            user.stateWork();
+            user.level = 93;
+            user.stateWork();
         }
     }
 
@@ -47,13 +61,13 @@ namespace DesignPatternTest
         }
     }
 
-    public class RED:Light
+    public class RED : Light
     {
         public override void change(TrafficLight trafficLight)
         {
             Console.WriteLine("RED...");
             sleep(1);
-            trafficLight.Light=new GREEN();
+            trafficLight.Light = new GREEN();
         }
     }
 
@@ -88,10 +102,127 @@ namespace DesignPatternTest
             set { _light = value; }
         }
 
-
         public void Change()
-       {
-           Light.change(this);
-       }
+        {
+            Light.change(this);
+        }
+    }
+
+    // 玩家類別，有一個等級的屬性
+    public class Player
+    {
+        public int level = 1; // 等級
+
+        /* 狀態處理，一般用 if else 的寫法
+        public void stateWork()
+        {
+            if (level >= 1 && level < 20)
+            {
+                Console.WriteLine("等級 {0} ({1})", level, "新手");
+            }
+            else if (level >= 20 && level < 50)
+            {
+                Console.WriteLine("等級 {0} ({1})", level, "老手");
+            }
+            else if (level >= 50 && level < 90)
+            {
+                Console.WriteLine("等級 {0} ({1})", level, "高手");
+            }
+            else if (level >= 90)
+            {
+                Console.WriteLine("等級 {0} ({1})", level, "神");
+            }
+        }
+        */
+
+        // 將狀態處理改為以下寫法 (狀態模式)
+        private StateContext state;
+
+        public Player()
+        {
+            // 初始化狀態處理的物件
+            setStateContext(new ConcreteState001());
+        }
+
+        // 設定狀態處理的物件
+        public void setStateContext(StateContext s)
+        {
+            state = s;
+        }
+
+        // 狀態處理，轉交由 StateContext 物件處理
+        public void stateWork()
+        {
+            state.stateWork(this);
+        }
+    }
+
+    // 狀態模式的抽象類別
+    public abstract class StateContext
+    {
+        public abstract void stateWork(Player user);
+    }
+
+    // 等級 1~20
+    public class ConcreteState001 : StateContext
+    {
+        public override void stateWork(Player user)
+        {
+            if (user.level < 20)
+            {
+                Console.WriteLine("等級 {0} ({1})", user.level, "新手");
+            }
+            else
+            {
+                user.setStateContext(new ConcreteState050());
+                user.stateWork();
+            }
+        }
+    }
+
+    // 等級 20~50
+    public class ConcreteState050 : StateContext
+    {
+        public override void stateWork(Player user)
+        {
+            if (user.level < 50)
+            {
+                Console.WriteLine("等級 {0} ({1})", user.level, "老手");
+            }
+            else
+            {
+                user.setStateContext(new ConcreteState090());
+                user.stateWork();
+            }
+        }
+    }
+
+    // 等級 50~90
+    public class ConcreteState090 : StateContext
+    {
+        public override void stateWork(Player user)
+        {
+            if (user.level < 90)
+            {
+                Console.WriteLine("等級 {0} ({1})", user.level, "高手");
+            }
+            else
+            {
+                user.setStateContext(new ConcreteStateMAX());
+                user.stateWork();
+            }
+        }
+    }
+
+    // 等級 90~
+    public class ConcreteStateMAX : StateContext
+    {
+        public override void stateWork(Player user)
+        {
+            if (user.level >= 90)
+            {
+                Console.WriteLine("等級 {0} ({1})", user.level, "神");
+            }
+        }
     }
 }
